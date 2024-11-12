d12f9027abe0285613a865d80f577efadb2ff85c
Mylena Viana Nunes <126629062+mylenosa@users.noreply.github.com>
2024-11-11 22:20:05 -0400

    atualiza atividade

---
+++ android/app/src/main/kotlin/com/example/aula211024my/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula211024my
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:aula211024my/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
