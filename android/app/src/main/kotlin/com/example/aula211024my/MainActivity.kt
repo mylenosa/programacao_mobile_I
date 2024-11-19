d25772f0886f32a254b3b705f863877420ee108d
mylenosa <mylenavnunes2004@gmail.com>
2024-11-18 20:12:48 -0400

    Organizando os arquivos

--- android/app/src/main/kotlin/com/example/aula211024my/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aula211024my
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:aula211024my/home_page.dart';
-
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
-}
